fdc0813ef465c8a89cd3247dad7d2f4671508c61
Sankalp Sangle <sankalpsangle@r-63-105-25-172.comp.nus.edu.sg>
2020-01-28 10:31:33 +0800

    Added Grid Position class

--- core.py
+++ core.py
@@ -1,18 +1,30 @@
 class Dashboard:
     def __init__(self, panels = None, properties = None):
+        if properties is None:
+            properties = Dashboard_Properties()
+        if panels is None:
+            panels = []
+
         self.properties          = properties
         self.panels              = panels
         self.json_representation = ""
 
     def get_json_string(self):
+        panelJSON = self.get_colective_panels_json()
+        return "{}, \"panels\":[{}]".format(self.properties.get_json_string(), panelJSON)
+
+    def get_colective_panels_json(self):
+        if self.panels is []:
+            return ""
+            
         panelJSON = ""
-        
         for panel in self.panels:
             panelJSON += "{" + panel.get_json_string() + "},"
-        
-        panelJSON = panelJSON[:-1]
 
-        return "{}, \"panels\":[{}]".format(self.properties.get_json_string(), panelJSON)
+        #Remove trailing comma
+        panelJSON = panelJSON[:-1]
+        
+        return panelJSON
 
 class Dashboard_Properties:
     def __init__(self, id = None, uid = None, title = None, timezone = None, schemaVersion = None, version = None, time = None):
@@ -43,24 +55,51 @@ class Dashboard_Properties:
     def get_json_string(self):
         return "\"id\": {},\"uid\": {},\"title\": \"{}\",\"timezone\": \"{}\",\"schemaVersion\": {},\"version\": {},\"time\":{}".format(self.id, self.uid, self.title, self.timezone, self.schemaVersion, self.version, "{" + self.time.get_json_string() + "}")
 
+class Grid_Position:
+    
+    def __init__(self, x = None, y = None, height = None, width = None):
+        if x is None:
+            x = 0
+        if y is None:
+            y = 0
+        if height is None:
+            height = 8
+        if width is None:
+            width = 12
+
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+
+    def get_json_string(self):
+        return "\"h\": {}, \"w\": {}, \"x\": {}, \"y\": {}".format(self.height, self.width, self.x, self.y) 
+
 class Panel:
-    def __init__(self, datasource = None, id = None, title = None, panelType = None):
+
+    GLOBAL_ID = 1
+
+    def __init__(self, datasource = None, id = None, title = None, panelType = None, gridPos = None):
         if datasource is None:
             datasource = "MySQL"
         if id is None:
-            id = "1"
+            id = str(Panel.GLOBAL_ID)
+            Panel.GLOBAL_ID += 1
         if title is None:
             title = "This is my sample panel title!"
         if panelType is None:
             panelType = "graph"
+        if gridPos is None:
+            gridPos = Grid_Position()
 
         self.datasource = datasource
         self.id = id
         self.title = title
         self.panelType = panelType
+        self.gridPos = gridPos
 
     def get_json_string(self):
-        return "\"datasource\": \"{}\",\"id\": {},\"title\": \"{}\",\"type\":\"{}\"".format(self.datasource, self.id, self.title, self.panelType)
+        return "\"datasource\": \"{}\",\"id\": {},\"title\": \"{}\",\"type\":\"{}\",\"gridPos\":{}".format(self.datasource, self.id, self.title, self.panelType, "{" + self.gridPos.get_json_string() + "}")
 
 class Time:
 
--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from core import Dashboard_Properties
 from core import Time
 from core import Dashboard
 from core import Panel
+from core import Grid_Position
 
-URL             = "http://localhost:3000/api/dashboards/db"
-API_KEY         = "eyJrIjoiQjBXRFZ4NHJGVWsySWtneFp4VlVEbjVabm1NM0p6VnMiLCJuIjoibXlLZXkiLCJpZCI6MX0="
+URL             = "http://172.26.191.244:3000/api/dashboards/db"
+API_KEY         = "eyJrIjoid3NxM2VGZHlpVGhVVzNKZEtsNktkNzV6S3NLSFRoNTAiLCJuIjoidGVzdCIsImlkIjoxfQ=="
 TIME_FROM       = "1998-04-03"
 TIME_TO         = "2020-04-03"
 DASHBOARD_TITLE = "This is a sample title!"
@@ -20,8 +21,12 @@ headers = {
 def main():
     time = Time(timeFrom=TIME_FROM, timeTo=TIME_TO)
     properties = Dashboard_Properties(title=DASHBOARD_TITLE, time=time)
+    
     panels = []
-    panels.append(Panel())
+    panels.append(Panel(title="Panel Number 1"))
+    panels.append(Panel(title="Panel Number 2"))
+    panels.append(Panel(title="Panel Number 3"))
+    
     dashboard = Dashboard(properties=properties, panels=panels)
     payload = get_final_payload(dashboard)
     # print(payload)
@@ -32,7 +37,7 @@ def send_dashboard_to_grafana(payload):
     print(response.text.encode('utf8'))
 
 def get_final_payload(dashboard):
-    payload = "{ \"dashboard\": {" + dashboard.get_json_string() + "}, \"overwrite\": false}"
+    payload = "{ \"dashboard\": {" + dashboard.get_json_string() + "}, \"overwrite\": true}"
     return payload
 
 if __name__ == "__main__":
