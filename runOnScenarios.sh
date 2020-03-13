45464708298401ac49e32af1004e0c6c4f6d100f
sankalp-sangle <sanku.sangle@gmail.com>
2020-03-13 11:37:45 +0800

    Added grep command to summarize output

--- runOnScenarios.sh
+++ runOnScenarios.sh
@@ -5,3 +5,5 @@ do
     echo "Testing Scenario :"microburst_incast_heavyhitter$i > results/microburst_incast_heavyhitter$i.txt
     python3 main.py microburst_incast_heavyhitter$i >> results/microburst_incast_heavyhitter$i.txt
 done
+
+grep -E 'Normalized|CONCLUDE' results/* > results/summary.txt
